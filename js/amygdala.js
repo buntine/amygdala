72bba6c6a763e44df4fda038b45113775e8ea149
Andrew Buntine <info@bunts.io>
2018-02-06 23:15:19 +1100

    100% canvas

--- js/amygdala.js
+++ js/amygdala.js
@@ -13,3 +13,14 @@
 //// Or clear on given keystroke
 //
 //// If no input after N seconds, randomize colors
+
+var canvas = document.getElementById("amygdala");
+var ctx = canvas.getContext("2d");
+
+ctx.canvas.width  = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
+
+ctx.beginPath();
+ctx.moveTo(50,20);
+ctx.bezierCurveTo(230, 30, 150, 60, 50, 100);
+ctx.stroke();
