0339a269f1a23e0f5b4d9aaa0fc56bac839cd642
Andrew Buntine <info@bunts.io>
2018-02-06 20:45:18 +1100

    Notes

--- js/amygdala.js
+++ js/amygdala.js
@@ -1,1 +1,15 @@
-console.log("works");
+// Painter class
+//// Has colors (2)
+//// Runs in setInterval
+//// Paints randomly on screen (length, width, speed, etc)
+
+// Main
+//// Set default colors
+//// Setup N painters
+//// Listen for keystroke (u and d)
+////// Update color in each painter
+//
+//// After N minutes, fade out canvas
+//// Or clear on given keystroke
+//
+//// If no input after N seconds, randomize colors
