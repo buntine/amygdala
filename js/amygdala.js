9692e55cb948befb82f8744b30c35e76a685515b
Andrew Buntine <info@bunts.io>
2018-02-07 21:28:54 +1100

    Setting up basic structure

--- js/amygdala.js
+++ js/amygdala.js
@@ -14,11 +14,41 @@
 //
 //// If no input after N seconds, randomize colors
 
+const MAXIMUM_WAIT_TIME = 500;
+
 var canvas = document.getElementById("amygdala");
 var ctx = canvas.getContext("2d");
 
 ctx.canvas.width  = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
+
+function random(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function Stroke(x, y, length, color) {
+  this.x = x;
+  this.y = y;
+  this.offset = 0;
+  this.color = color;
+  this.length = length;
+}
+
+function Painter(x, y, hiColor, loColor) {
+  this.x = x;
+  this.y = y;
+  this.hiColor = hiColor;
+  this.loColor = loColor;
+  this.strokes = [];
+}
+
+Painter.prototype.reset = () => {
+  this.strokes = [];
+};
+
+Painter.prototype.scheduleStroke = () => {
+  setTimeout(this.createStroke, random(MAX_WAIT_TIME));
+};
 
 ctx.beginPath();
 ctx.moveTo(50,20);
