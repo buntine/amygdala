17ff3ce8344a9ca85d0fb37ab7ffe10795e08d69
Andrew Buntine <info@bunts.io>
2018-02-08 21:50:10 +1100

    Added update functions

--- js/amygdala.js
+++ js/amygdala.js
@@ -20,6 +20,7 @@ const MIN_HEIGHT = 70;
 const MAX_STROKE_WIDTH = 800;
 const MIN_STROKE_WIDTH = 300;
 const STROKE_VARIANCE = 40;
+const LENGTH_PER_STROKE = 5;
 
 var canvas = document.getElementById("amygdala");
 var ctx = canvas.getContext("2d");
@@ -39,9 +40,24 @@ function Stroke(x, y, length, color) {
   this.color = color;
 }
 
-function Painter(x, y, hiColor, loColor) {
-  this.x = x;
-  this.y = y;
+Stroke.prototype.update() {
+  const nextOffset = this.offset + LENGTH_PER_FRAME;
+
+  ctx.beginPath();
+  ctx.moveTo(this.x + this.offset, this.y);
+  ctx.lineTo(this.x + nextOffset, this.y);
+  ctx.stroke();
+
+  this.offset = nextOffset;
+
+  return this.isFinished();
+}
+
+Stroke.prototype.isFinished() {
+  return this.offset > this.length;
+}
+
+function Painter(hiColor, loColor) {
   this.hiColor = hiColor;
   this.loColor = loColor;
   this.strokes = [];
@@ -66,16 +82,23 @@ Painter.prototype.scheduleStroke = () => {
 Painter.prototype.createStroke = () => {
   const height = random(MAX_HEIGHT, MIN_HEIGHT);
   const length = random(MAX_STROKE_WIDTH, MIN_STROKE_WIDTH);
+  const x = random(window.innerWidth - length)
+  const y = random(window.innerHeight - height)
 
   for (const i = 0; i < height; i++) {
-    const length = random(length, length - STROKE_VARIANCE);
+    const strokeLength = random(length, length - STROKE_VARIANCE);
 
-    this.strokes.push(new Stroke(this.x, this.y + i, length, "lerpColor between hiColor and loColor"));
+    this.strokes.push(new Stroke(x, y + i, strokeLength, "lerpColor between hiColor and loColor"));
   }
 };
 
+Painter.prototype.update = () => {
+  if (this.strokes.length > 0) {
+    const remaining = this.strokes.map(s => s.update());
 
-ctx.beginPath();
-ctx.moveTo(50,20);
-ctx.bezierCurveTo(230, 30, 150, 60, 50, 100);
-ctx.stroke();
+    if (remaining.every(r => !!r)) {
+      this.reset();
+      this.scheduleStroke();
+    }
+  }
+}
