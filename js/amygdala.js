d0e2234a26e2ee72ded354f0391a474afe31484e
Andrew Buntine <info@bunts.io>
2018-02-13 21:24:12 +1100

    Removed stepping through RGBs with set colors that are cycled through

--- js/amygdala.js
+++ js/amygdala.js
@@ -94,32 +94,13 @@ class HorizontalStroke {
 }
 
 class Painter {
-  constructor(hiColor, loColor) {
-    this.hiColor = hiColor;
-    this.loColor = loColor;
+  constructor(nextColorKey, prevColorKey) {
+    this.color = new Color(nextColorKey, prevColorKey);
     this.strokes = [];
   }
 
   reset() {
     this.strokes = [];
-  }
-
-  setHiColor(color) {
-    this.hiColor = color;
-  }
-
-  setLoColor(color) {
-    this.loColor = color;
-  }
-
-  lerpColor(amount) {
-    const hi = this.hiColor;
-    const lo = this.loColor;
-    const rr = hi.r + amount * (lo.r - hi.r);
-    const rg = hi.g + amount * (lo.g - hi.g);
-    const rb = hi.b + amount * (lo.b - hi.b);
-
-    return `rgb(${rr.toFixed()}, ${rg.toFixed()}, ${rb.toFixed()})`;
   }
 
   createStrokes() {
@@ -134,7 +115,7 @@ class Painter {
       const strokeLength = random(length, length - STROKE_VARIANCE);
 
       this.strokes.push(
-        new kind(x, y, i, strokeLength, tilt, this.lerpColor(Math.random())));
+        new kind(x, y, i, strokeLength, tilt, this.color.lerpColor(Math.random())));
     }
   }
 
@@ -152,37 +133,51 @@ class Painter {
   }
 }
 
-class Color {
-  constructor(r, g, b) {
-    this.r = r[0];
-    this.g = g[0];
-    this.b = b[0];
+const COLORS = [
+  [[100, 91, 140], [80, 67, 91]],
+  [[133, 121, 170], [140, 97, 121]],
+];
 
-    window.addEventListener("keydown", e => {
-      switch (e.key) {
-        case r[1]:
-          this.r = this.r + 1;
-          break;
-        case g[1]:
-          this.g = this.g + 1;
-          break;
-        case b[1]:
-          this.b = this.b + 1;
-        default:
+class Color {
+  constructor(nextColorKey, prevColorKey) {
+    this.high = {r: 200, g: 89, b: 126};
+    this.low = {r: 140, g: 59, b: 88};
+    this.i = 0;
+
+    window.addEventListener("keydown", (e) => {
+      if (e.key === nextColorKey) {
+        this.nextColor();
+      } else if (e.key === prevColorKey) {
+        this.prevColor();
       }
     });
+  }
+
+  lerpColor(amount) {
+    const color = COLORS[this.i];
+    const hi = color[0];
+    const lo = color[1];
+    const rr = hi[0] + amount * (lo[0] - hi[0]);
+    const rg = hi[1] + amount * (lo[1] - hi[1]);
+    const rb = hi[2] + amount * (lo[2] - hi[2]);
+
+    return `rgb(${rr.toFixed()}, ${rg.toFixed()}, ${rb.toFixed()})`;
+  }
+
+  nextColor() {
+    this.i = (this.i + 1) % COLORS.length;
+  }
+
+  prevColor() {
+    const nextI = this.i - 1;
+    this.i = nextI < 0 ? COLORS.length - 1 : nextI;
   }
 }
 
 const painters = [
-  new Painter(
-    new Color([255, 'a'], [67, 'b'], [102, 'c']),
-    new Color([205, 'd'], [17, 'e'], [52, 'f']),
-  ),
-  new Painter(
-    new Color([255, 'g'], [67, 'h'], [102, 'i']),
-    new Color([205, 'j'], [17, 'k'], [52, 'l']),
-  ),
+  new Painter('a', 'b'),
+  new Painter('c', 'd'),
+  new Painter('e', 'f'),
 ];
 
 function main() {
