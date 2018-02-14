71896a3e8a0be4d430b967ff0683acd9ff9659f0
Andrew Buntine <info@bunts.io>
2018-02-14 23:21:21 +1100

    Moved constants out

--- typescript/amygdala.ts
+++ typescript/amygdala.ts
@@ -1,23 +1,11 @@
-const MAX_WAIT_TIME = 500;
-const MAX_HEIGHT = 300;
-const MIN_HEIGHT = 70;
-const MAX_STROKE_WIDTH = 800;
-const MIN_STROKE_WIDTH = 300;
-const MAX_STROKE_HEIGHT = 4;
-const STROKE_VARIANCE = 50;
-const MIN_SPEED = 7;
-const MAX_SPEED = 12;
-const HORIZ_PROBABILITY = 0.8;
-const TILT_FACTOR = 3;
-const BLUR_SIZE = 10;
-const NO_INPUT_WAIT_TIME = 60000;
+import * as c from "./constants";
 
 const canvas = <HTMLCanvasElement>document.getElementById("amygdala");
 const ctx = canvas.getContext("2d");
 
 ctx.canvas.width  = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
-ctx.shadowBlur = BLUR_SIZE;
+ctx.shadowBlur = c.BLUR_SIZE;
 
 const random = function(max, min = 0) {
   return Math.floor(Math.random() * Math.floor(max - min)) + min;
@@ -55,17 +43,17 @@ class VerticalStroke {
 
   constructor(x : number, y : number, offset : number, length : number, tilt : number, color : string) {
     this.x = x + offset;
-    this.y = y + random(STROKE_VARIANCE);
+    this.y = y + random(c.STROKE_VARIANCE);
     this.xOffset = 0;
     this.yOffset = 0;
     this.length = length;
     this.tilt = tilt;
     this.color = color;
-    this.lineWidth = random(MAX_STROKE_HEIGHT, 1);
+    this.lineWidth = random(c.MAX_STROKE_HEIGHT, 1);
   }
 
   update() {
-    const nextYOffset = this.yOffset + random(MAX_SPEED, MIN_SPEED);
+    const nextYOffset = this.yOffset + random(c.MAX_SPEED, c.MIN_SPEED);
     const nextXOffset = this.xOffset + this.tilt;
 
     ctx.beginPath();
@@ -87,7 +75,6 @@ class VerticalStroke {
   }
 }
 
-
 class HorizontalStroke {
   x : number;
   y : number;
@@ -99,18 +86,18 @@ class HorizontalStroke {
   lineWidth : number;
 
   constructor(x : number, y : number, offset : number, length : number, tilt : number, color : string) {
-    this.x = x + random(STROKE_VARIANCE);
+    this.x = x + random(c.STROKE_VARIANCE);
     this.y = y + offset;
     this.xOffset = 0;
     this.yOffset = 0;
     this.length = length;
     this.tilt = tilt;
     this.color = color;
-    this.lineWidth = random(MAX_STROKE_HEIGHT, 1);
+    this.lineWidth = random(c.MAX_STROKE_HEIGHT, 1);
   }
 
   update() {
-    const nextXOffset = this.xOffset + random(MAX_SPEED, MIN_SPEED);
+    const nextXOffset = this.xOffset + random(c.MAX_SPEED, c.MIN_SPEED);
     const nextYOffset = this.yOffset + this.tilt;
 
     ctx.beginPath();
@@ -146,15 +133,15 @@ class Painter {
   }
 
   createStrokes() {
-    const height = random(MAX_HEIGHT, MIN_HEIGHT);
-    const length = random(MAX_STROKE_WIDTH, MIN_STROKE_WIDTH);
-    const tilt = random(TILT_FACTOR, -TILT_FACTOR);
+    const height = random(c.MAX_HEIGHT, c.MIN_HEIGHT);
+    const length = random(c.MAX_STROKE_WIDTH, c.MIN_STROKE_WIDTH);
+    const tilt = random(c.TILT_FACTOR, -c.TILT_FACTOR);
     const x = random(window.innerWidth - length);
     const y = random(window.innerHeight - height);
-    const kind = Math.random() > HORIZ_PROBABILITY ? VerticalStroke : HorizontalStroke;
+    const kind = Math.random() > c.HORIZ_PROBABILITY ? VerticalStroke : HorizontalStroke;
 
     for (let i = 0; i < height; i++) {
-      const strokeLength = random(length, length - STROKE_VARIANCE);
+      const strokeLength = random(length, length - c.STROKE_VARIANCE);
 
       this.strokes.push(
         new kind(x, y, i, strokeLength, tilt, this.color.lerpColor(Math.random())));
@@ -174,11 +161,6 @@ class Painter {
     }
   }
 }
-
-const COLORS = [
-  [[100, 91, 140], [80, 67, 91]],
-  [[133, 121, 170], [140, 97, 121]],
-];
 
 class Color {
   high : RGB;
@@ -202,7 +184,7 @@ class Color {
   }
 
   lerpColor(amount) {
-    const color = COLORS[this.i];
+    const color = c.COLORS[this.i];
     const hi = color[0];
     const lo = color[1];
     const rr = hi[0] + amount * (lo[0] - hi[0]);
@@ -213,13 +195,13 @@ class Color {
   }
 
   nextColor() {
-    this.i = (this.i + 1) % COLORS.length;
+    this.i = (this.i + 1) % c.COLORS.length;
     this.scheduleChange();
   }
 
   prevColor() {
     const nextI = this.i - 1;
-    this.i = nextI < 0 ? COLORS.length - 1 : nextI;
+    this.i = nextI < 0 ? c.COLORS.length - 1 : nextI;
     this.scheduleChange();
   }
 
@@ -228,7 +210,7 @@ class Color {
       clearTimeout(this.pendingChange);
     }
 
-    this.pendingChange = setTimeout(() => this.nextColor(), NO_INPUT_WAIT_TIME);
+    this.pendingChange = setTimeout(() => this.nextColor(), c.NO_INPUT_WAIT_TIME);
   }
 }
 
---
+++ typescript/constants.ts
@@ -0,0 +1,18 @@
+export const MAX_WAIT_TIME = 500;
+export const MAX_HEIGHT = 300;
+export const MIN_HEIGHT = 70;
+export const MAX_STROKE_WIDTH = 800;
+export const MIN_STROKE_WIDTH = 300;
+export const MAX_STROKE_HEIGHT = 4;
+export const STROKE_VARIANCE = 50;
+export const MIN_SPEED = 7;
+export const MAX_SPEED = 12;
+export const HORIZ_PROBABILITY = 0.8;
+export const TILT_FACTOR = 3;
+export const BLUR_SIZE = 10;
+export const NO_INPUT_WAIT_TIME = 60000;
+export const COLORS = [
+  [[100, 91, 140], [80, 67, 91]],
+  [[133, 121, 170], [140, 97, 121]],
+];
+
